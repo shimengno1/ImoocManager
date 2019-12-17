f1598f0431185d69f3a01a9f57439c8a00c279a2
shimengno1 <2577253445@qq.com>
2019-12-17 19:03:19 +0800

    Update index.js

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,17 +1,20 @@
 import JsonP from 'jsonp'
 export default class Axios {
-  static jsonp (options) {
+  static jsonp(options) {
     return new Promise((resolve, reject) => {
-      JsonP(options.url, {
-        param: 'callback'
-      }, function (err, response) {
-        //to-do
-        if (response.status === "success") {
-          resolve(response);
-        } else {
-          reject(response.message);
+      JsonP(
+        options.url,
+        {
+          param: 'callback'
+        },
+        function(err, response) {
+          if (response.status === 'success') {
+            resolve(response)
+          } else {
+            reject(response.message)
+          }
         }
-      })
+      )
     })
   }
 }
