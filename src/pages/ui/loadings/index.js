363bb3f48424d79cd7f910afc3d3f3ebba8bc25c
shimengno1 <2577253445@qq.com>
2019-12-17 18:36:43 +0800

    Update index.js

--- src/pages/ui/loadings/index.js
+++ src/pages/ui/loadings/index.js
@@ -3,7 +3,8 @@ import { Card, Spin, Icon, Alert } from 'antd'
 import './../ui.less'
 export default class Loadings extends React.Component {
   render() {
-    const icon = <Icon type="plus" style={{ fontSize: 24 }}></Icon>
+    const icon = <Icon type="loading" style={{ fontSize: 24 }}></Icon>
+    const iconLoading = <Icon type="loading" style={{ fontSize: 24 }}></Icon>
     return (
       <div>
         <Card title="Spin用法" className="card-wrap">
@@ -12,8 +13,33 @@ export default class Loadings extends React.Component {
           <Spin size="large" />
           <Spin indicator={icon} style={{ marginLeft: 10 }}></Spin>
         </Card>
-        <Card title="内容遮罩">
-          <Alert type="info"></Alert>
+        <Card title="内容遮罩" className="card-wrap">
+          <Alert
+            type="info"
+            message="React"
+            description="欢迎来到React高级实战课程"
+          />
+          <Spin>
+            <Alert
+              type="warning"
+              message="React"
+              description="欢迎来到React高级实战课程"
+            />
+          </Spin>
+          <Spin tip="加载中...">
+            <Alert
+              type="warning"
+              message="React"
+              description="欢迎来到React高级实战课程"
+            />
+          </Spin>
+          <Spin indicator={iconLoading}>
+            <Alert
+              type="warning"
+              message="React"
+              description="欢迎来到React高级实战课程"
+            />
+          </Spin>
         </Card>
       </div>
     )
