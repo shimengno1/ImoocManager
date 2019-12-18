3b276b9255af6e9f5595a1ae6b48df6231f5af6f
shimengno1 <2577253445@qq.com>
2019-12-18 23:23:59 +0800

    simon

---
+++ src/pages/city/index.js
@@ -0,0 +1,95 @@
+import React from "react";
+import axios from "./../../axios/index";
+import utils from "./../../utils/utils";
+import { Card, Form, Select, Button, Table } from "antd";
+const FormItem = Form.Item;
+const Option = Select.Option;
+export default class City extends React.Component {
+  // 开通城市
+  handleOpenCity = () => {};
+  render() {
+      const columns=[{
+          title:'城市ID',
+          dataIndex:'id'
+      },{
+        title:'城市名称',
+        dataIndex:'name'
+    },{
+        title:'用车模式',
+        dataIndex:'mode'
+    },{
+        title:'营运模式',
+        dataIndex:'op_mode'
+    },{
+        title:'授权加盟商',
+        dataIndex:'franchisee_name'
+    }]
+    return (
+      <div>
+        <Card>
+          <FliterForm />
+        </Card>
+        <Card>
+          <Button type="primary" onClick={this.handleOpenCity}>
+            开通城市
+          </Button>
+        </Card>
+        <Table columns={columns}></Table>
+      </div>
+    );
+  }
+}
+// 表单元素
+class FliterForm extends React.Component {
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form layout="inline">
+        <FormItem label="城市">
+          {getFieldDecorator("city_id")(
+            <Select placeholder="全部" style={{ width: 80 }}>
+              <Option value="">全部</Option>
+              <Option value="1">北京市</Option>
+              <Option value="2">天津市</Option>
+              <Option value="3">深圳市</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem label="用车模式">
+          {getFieldDecorator("mode")(
+            <Select placeholder="全部" style={{ width: 80 }}>
+              <Option value="">全部</Option>
+              <Option value="1">指定停车点模式</Option>
+              <Option value="2">禁停区模式</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem label="营运模式">
+          {getFieldDecorator("op_mode")(
+            <Select placeholder="全部" style={{ width: 80 }}>
+              <Option value="">全部</Option>
+              <Option value="1">自营</Option>
+              <Option value="2">加盟</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem label="加盟商授权状态">
+          {getFieldDecorator("auth_status")(
+            <Select placeholder="全部" style={{ width: 80 }}>
+              <Option value="">全部</Option>
+              <Option value="1">已授权</Option>
+              <Option value="2">未授权</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem>
+          <Button type="primary" style={{ margin: "0 20px" }}>
+            查询
+          </Button>
+          <Button>重置</Button>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+FliterForm = Form.create({})(FliterForm);
--- src/router.js
+++ src/router.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { HashRouter, Route, Switch } from 'react-router-dom'
-import Admin from './admin'
-import App from './App'
-import Login from './pages/login/index'
-import NoMatch from './pages/nomatch/index'
-import Buttons from './pages/ui/buttons/index'
-import Modals from './pages/ui/modals/index'
-import Loadings from './pages/ui/loadings/index'
-import Notifications from './pages/ui/notifications/notification'
+import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Admin from "./admin";
+import App from "./App";
+import Login from "./pages/login/index";
+import NoMatch from "./pages/nomatch/index";
+import Buttons from "./pages/ui/buttons/index";
+import Modals from "./pages/ui/modals/index";
+import Loadings from "./pages/ui/loadings/index";
+import Notifications from "./pages/ui/notifications/notification";
+import City from "./pages/city/index";
 export default class IRouter extends React.Component {
   render() {
     return (
@@ -22,6 +23,7 @@ export default class IRouter extends React.Component {
                   <Route path="/admin/ui/buttons" component={Buttons}></Route>
                   <Route path="/admin/ui/modals" component={Modals}></Route>
                   <Route path="/admin/ui/loadings" component={Loadings}></Route>
+                  <Route path="/admin/city" component={City}></Route>
                   <Route
                     path="/admin/ui/notification"
                     component={Notifications}
@@ -33,6 +35,6 @@ export default class IRouter extends React.Component {
           ></Route>
         </App>
       </HashRouter>
-    )
+    );
   }
 }
